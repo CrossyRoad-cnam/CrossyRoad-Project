3d55d5b6045170833df5b1c13c4316cbdb13ec98
Winness R <winness.rakotozafy@outlook.fr>
2024-02-01 20:35:58 +0100

    NEW - Add terrain holder

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,6 +6,7 @@ public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private List<TerrainData> terrainData= new List<TerrainData>();
     [SerializeField] private int maxTerrainCount;
+    [SerializeField] private Transform terrainHolder;
 
     private List<GameObject> currentTerrains = new List<GameObject>();
     private Vector3 currentPosition = Vector3.zero;
@@ -14,32 +15,36 @@ public class TerrainGenerator : MonoBehaviour
     {
         for (int i = 0; i < maxTerrainCount; i++)
         {
-            SpawnTerrain();
+            SpawnTerrain(true);
         }
+        maxTerrainCount += currentTerrains.Count;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            SpawnTerrain();
+            SpawnTerrain(false);
         }
     }
 
-    private void SpawnTerrain()
+    private void SpawnTerrain(bool isStart)
     {
         int whichTerrain = Random.Range(0, terrainData.Count);
         int terrainInSuccession = Random.Range(1, terrainData[whichTerrain].maxInSuccession);
 
         for (int i = 0; i < terrainInSuccession; i++)
         {
-            GameObject terrain = Instantiate(terrainData[whichTerrain].terrain, currentPosition, Quaternion.identity);
+            GameObject terrain = Instantiate(terrainData[whichTerrain].terrain, currentPosition, Quaternion.identity, terrainHolder);
             currentTerrains.Add(terrain);
-            if (currentTerrains.Count > maxTerrainCount)
+            if (!isStart)
             {
-                Destroy(currentTerrains[0]);
-                currentTerrains.RemoveAt(0);
-            }
+                if (currentTerrains.Count > maxTerrainCount)
+                {
+                    Destroy(currentTerrains[0]);
+                    currentTerrains.RemoveAt(0);
+                }
+            }          
             currentPosition.x++;
         }
     }
