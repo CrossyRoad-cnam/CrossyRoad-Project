5bc800ac11331f7407a53ab533a8fe10cb1db214
Winness R <winness.rakotozafy@outlook.fr>
2024-02-01 21:52:27 +0100

    FEAT - Add player forward movement -> need fix animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator animator;
+    private bool isHopping;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !isHopping)
+        {
+            animator.SetTrigger("hop");
+            isHopping = true;
+            Debug.Log(transform.position);
+            if(transform.position.z % 1 == 0)
+            {
+                Debug.Log("On Grid Space");
+                transform.Translate(new Vector3(1, 0, 0));
+            }
+        }
+
+    }
+
+    public void FinishHop()
+    {
+        isHopping = false;
+    }
 }
