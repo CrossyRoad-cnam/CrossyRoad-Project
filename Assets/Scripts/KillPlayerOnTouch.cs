4f4b6ece238ea661461406f2e0c5cf905c4e5623
WRKT <winness.rakotozafy@outlook.fr>
2024-04-03 13:19:58 +0200

    FIX - Enable kill player from ennemies (onTrigger)

--- Assets/Scripts/KillPlayerOnTouch.cs
+++ Assets/Scripts/KillPlayerOnTouch.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour
 {
-    public void OnCollisionEnter(Collision collision)
+    // Handle collision when the player collides with the obstacle
+    private void OnCollisionEnter(Collision collision)
     {
         Player player = collision.gameObject.GetComponent<Player>();
         if (player != null)
         {
-            Debug.Log("GAME OVER");
+            Debug.Log("Player collided with obstacle. GAME OVER");
+            Destroy(player.gameObject);
+        }
+    }
+
+    // Handle collision when the obstacle collides with the player
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            Debug.Log("Obstacle collided with player. GAME OVER");
             Destroy(player.gameObject);
         }
     }
