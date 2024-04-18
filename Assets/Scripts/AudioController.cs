bcb925a113da90e8ee1d335e5004182eddeb26b7
Dinholu <alizeehett@hotmail.fr>
2024-04-18 13:06:24 +0200

    ADD - ajout de musique dans le lobby

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    public AudioClip sound;
+
+    [Range(0, 1f)]
+    public float volume = 1f;
+
+    [Range(0.1f, 2.5f)]
+    public float pitch = 1f;
+
+    private AudioSource audioSource;
+
+    public void Awake()
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = sound;
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+        audioSource.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
