41425845199ee8737ce71f2094397c2e9399f09f
Jérémie <jeremie.moser@gmail.com>
2024-04-18 13:35:13 +0200

    FEAT - Player lands on StartTerrains

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
+using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour
 {
@@ -8,23 +13,54 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private List<TerrainData> terrainDatas = new List<TerrainData>();
     [SerializeField] private int maxTerrainCount;
     [SerializeField] private Transform terrainHolder;
-    [SerializeField] private GameObject StartTerrain;
+    /// <summary>
+    /// Indicates which terrain the player will spawn on
+    /// </summary>
+    [SerializeField] private int playerStartPos;
+    [SerializeField] private List<GameObject> StartTerrains;
 
     [HideInInspector] public Vector3 currentPosition = new Vector3(0, 0, 0);
     private List<GameObject> currentTerrains = new List<GameObject>();
 
     private void Start()
     {
-        SpawnTerrain(StartTerrain);
+        DefineStartPosition();
+        SpawnStartingTerrains();
         for (int i = 0; i < maxTerrainCount; i++)
         {
             SpawnTerrain(true, new Vector3(0, 0, 0));
         }
     }
-    public void SpawnTerrain(GameObject terrain)
+
+    private void DefineStartPosition()
+    {
+        if (playerStartPos <= StartTerrains.Count - 1 && playerStartPos >= 0)
+        {
+            currentPosition.x = currentPosition.x - playerStartPos;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("playerStartPos", playerStartPos, "La position de départ du joueur est actuellement située en dehors sa zone d'apparition (voir StartTerrains) !");
+        }
+    }
+
+    public void SpawnStartingTerrains()
+    {
+        int i = 0;
+        foreach (var terrain in StartTerrains)
+        {
+            SpawnTerrain(terrain, i++ - playerStartPos);
+        }
+    }
+    /// <summary>
+    /// Spawns a terrain to a dedicated x location
+    /// </summary>
+    /// <param name="terrain"></param>
+    /// <param name="position_X"></param>
+    public void SpawnTerrain(GameObject terrain, int position_X)
     {
         currentTerrains.Add(terrain);
-        Instantiate(StartTerrain,currentPosition, Quaternion.identity, terrainHolder);
+        Instantiate(terrain, currentPosition, Quaternion.identity, terrainHolder);
         currentPosition.x++;
     }
 
