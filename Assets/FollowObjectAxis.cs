30457488236cd5f210432a7ee1d76adbba2bb4e0
Jérémie <jeremie.moser@gmail.com>
2024-04-19 10:30:45 +0200

    FEAT - Adding map edge death

---
+++ Assets/FollowObjectAxis.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObjectAxis : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private bool followXAxis;
+    [SerializeField] private bool followYAxis;
+    [SerializeField] private bool followZAxis;
+
+    void Update()
+    {
+        FollowPlayerOnAxis();
+    }
+
+    /// <summary>
+    /// Follow the player on the checked axis
+    /// </summary>
+    private void FollowPlayerOnAxis()
+    {
+        transform.position = new Vector3(followXAxis ? player.transform.position.x : transform.position.x, followYAxis ? player.transform.position.y : transform.position.y, followZAxis ? player.transform.position.z : transform.position.z);
+    }
+}
