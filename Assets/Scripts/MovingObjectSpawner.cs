5f7bf90187266f69b0e1eec76380343db1c299cd
Dinholu <alizeehett@hotmail.fr>
2024-04-27 16:59:33 +0200

    Merge branch 'train-jeremie' into test-winness 🤲🤲🤲🤲

--- Assets/Scripts/MovingObjectSpawner.cs
+++ Assets/Scripts/MovingObjectSpawner.cs
@@ -9,6 +9,13 @@ public class MovingObjectSpawner : MonoBehaviour
     [SerializeField] private float minSeparationTime;
     [SerializeField] private float maxSeparationTime;
     [SerializeField] private bool isRightSide;
+    /// <summary>
+    /// D�termine de cb de temps d'avance l'�v�nement 'ObjectIncoming' est d�clench�
+    /// </summary>
+    [SerializeField] float EventThrowAdvancePercentage = 0.3f;
+    public delegate void ObjectIncomingEventHandler();
+    public event ObjectIncomingEventHandler ObjectIncoming;
+
 
     private void Start()
     {
@@ -19,8 +26,18 @@ public class MovingObjectSpawner : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
-            GameObject newObject = Instantiate(movingObject, spawnPosition.position, Quaternion.identity);
+            /// MODIFICATION JEREMIE
+            /// J'ai d�coup� ici le temps d'attente afin que l'�v�nement 'ObjectIncoming' (objet en mouvement en approche !) puisse �tre envoy� en avance
+            /// Ainsi pour les rails, je sais � l'avance qu'un train arrive et je peux donc faire clignoter les feux du terrain
+            /// MODIFICATION JEREMIE
+            float seconds = Random.Range(minSeparationTime, maxSeparationTime);
+            yield return new WaitForSeconds(seconds * (1 - EventThrowAdvancePercentage));
+            ObjectIncoming?.Invoke();
+            yield return new WaitForSeconds(seconds * EventThrowAdvancePercentage);
+            GameObject newObject = Instantiate(movingObject, spawnPosition.position, movingObject.transform.rotation);
+
+            // Allow the 'movingObject' to rotate, according to the 'spawnPos' values (usage: train orientation, yAngle = 180�)
+            newObject.transform.Rotate(spawnPosition.rotation.x, spawnPosition.rotation.y, spawnPosition.rotation.z);
             newObject.transform.SetParent(transform, true); // Hotfix pour que les objets soient détruites avec le terrain
 
             if (!isRightSide)
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
+using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour
 {
@@ -8,6 +13,11 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private List<TerrainData> terrainDatas;
     [SerializeField] private int maxTerrainCount;
     [SerializeField] private Transform terrainHolder;
+    /// <summary>
+    /// Indicates which terrain the player will spawn on
+    /// </summary>
+    [SerializeField] private int playerStartPos;
+    [SerializeField] private List<GameObject> StartTerrains;
 
     [HideInInspector] public Vector3 currentPosition = new Vector3(0, 0, 0);
 
@@ -16,10 +26,44 @@ public class TerrainGenerator : MonoBehaviour
 
     private void Start()
     {
+        DefineStartPosition();
+        SpawnStartingTerrains();
         for (int i = 0; i < maxTerrainCount; i++)
         {
             SpawnTerrain(true, new Vector3(0, 0, 0));
         }
+    }
+
+    private void DefineStartPosition()
+    {
+        if (playerStartPos <= StartTerrains.Count - 1 && playerStartPos >= 0)
+        {
+            currentPosition.x = currentPosition.x - playerStartPos;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("playerStartPos", playerStartPos, "La position de d�part du joueur est actuellement situ�e en dehors sa zone d'apparition (voir StartTerrains) !");
+        }
+    }
+
+    public void SpawnStartingTerrains()
+    {
+        int i = 0;
+        foreach (var terrain in StartTerrains)
+        {
+            SpawnTerrain(terrain, i++ - playerStartPos);
+        }
+    }
+    /// <summary>
+    /// Spawns a terrain to a dedicated x location
+    /// </summary>
+    /// <param name="terrain"></param>
+    /// <param name="position_X"></param>
+    public void SpawnTerrain(GameObject terrain, int position_X)
+    {
+        currentTerrains.Add(terrain);
+        Instantiate(terrain, currentPosition, Quaternion.identity, terrainHolder);
+        currentPosition.x++;
     }
 
     public void SpawnTerrain(bool isStart, Vector3 playerPosition)
