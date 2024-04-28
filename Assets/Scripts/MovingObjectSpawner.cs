004ea96e06c970a1185eaf286504c1545b71ed17
Winness R <winness.rakotozafy@outlook.fr>
2024-04-28 22:48:06 +0200

    FEAT - add new model + slightly change animation

--- Assets/Scripts/MovingObjectSpawner.cs
+++ Assets/Scripts/MovingObjectSpawner.cs
@@ -9,6 +9,9 @@ public class MovingObjectSpawner : MonoBehaviour
     [SerializeField] private float minSeparationTime;
     [SerializeField] private float maxSeparationTime;
     [SerializeField] private bool isRightSide;
+    /// <summary>
+    /// D�termine de cb de temps d'avance l'�v�nement 'ObjectIncoming' est d�clench�
+    /// </summary>
     [SerializeField] float EventThrowAdvancePercentage = 0.3f;
     public delegate void ObjectIncomingEventHandler();
     public event ObjectIncomingEventHandler ObjectIncoming;
@@ -24,10 +27,18 @@ public class MovingObjectSpawner : MonoBehaviour
         float seconds = 1f;
         while (true)
         {
+            /// MODIFICATION JEREMIE
+            /// J'ai d�coup� ici le temps d'attente afin que l'�v�nement 'ObjectIncoming' (objet en mouvement en approche !) puisse �tre envoy� en avance
+            /// Ainsi pour les rails, je sais � l'avance qu'un train arrive et je peux donc faire clignoter les feux du terrain
+            /// MODIFICATION JEREMIE 
+            
             yield return new WaitForSeconds(seconds * (1 - EventThrowAdvancePercentage));
             ObjectIncoming?.Invoke();
             yield return new WaitForSeconds(seconds * EventThrowAdvancePercentage);
             GameObject newObject = Instantiate(movingObject, spawnPosition.position, movingObject.transform.rotation);
+
+            // Allow the 'movingObject' to rotate, according to the 'spawnPos' values (usage: train orientation, yAngle = 180�)
+            newObject.transform.Rotate(spawnPosition.rotation.x, spawnPosition.rotation.y, spawnPosition.rotation.z);
             newObject.transform.SetParent(transform, true); // Hotfix pour que les objets soient détruites avec le terrain
 
             if (!isRightSide)
@@ -35,12 +46,15 @@ public class MovingObjectSpawner : MonoBehaviour
                 newObject.transform.Rotate(0, 180, 0);
             }
             seconds = Random.Range(minSeparationTime, maxSeparationTime);
+
+
             StartCoroutine(DestroyOutOfBounds(newObject));
         }
     }
+
     private IEnumerator DestroyOutOfBounds(GameObject obj)
     {
-        float terrainSize = transform.localScale.z;
+        float terrainSize = 60f;
         float minTerrainZ = spawnPosition.position.z - terrainSize;
         float maxTerrainZ = spawnPosition.position.z + terrainSize;
 
