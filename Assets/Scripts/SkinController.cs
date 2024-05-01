449742bc60600dca34ffca4a17a3e45382bb182d
Winness R <winness.rakotozafy@outlook.fr>
2024-05-01 17:55:29 +0200

    FEAT - selection skin menu displayed

--- Assets/Scripts/SkinController.cs
+++ Assets/Scripts/SkinController.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class SkinController : MonoBehaviour
 {
     public GameObject[] skins;
     public Transform previewPosition;
+    public TextMeshProUGUI skinName;
     private GameObject currentPreview;
     private int currentIndex = 0;
 
-    void Start()
+    private void Start()
     {
+        PreviewSkin(currentIndex);
+        DisplaySkinName();
+    }
+    private void Update()
+    {
+        DisplaySkinName();
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            NextSkin();
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            PreviousSkin();
+        if (Input.GetKeyDown(KeyCode.Space))
+            SelectSkin();
+    }
+
+    public void NextSkin()
+    {
+        currentIndex++;
+        if (currentIndex >= skins.Length) 
+            currentIndex = 0;
         PreviewSkin(currentIndex);
     }
 
-    public void ChangeSkin(int change)
+    public void PreviousSkin()
     {
-        currentIndex += change;
-        if (currentIndex >= skins.Length) currentIndex = 0;
-        else if (currentIndex < 0) currentIndex = skins.Length - 1;
-
+        currentIndex--;
+        if (currentIndex < 0) 
+            currentIndex = skins.Length - 1;
         PreviewSkin(currentIndex);
     }
 
     private void PreviewSkin(int index)
     {
-        if (currentPreview != null) Destroy(currentPreview);
+        if (currentPreview != null) 
+            Destroy(currentPreview);
         currentPreview = Instantiate(skins[index], previewPosition.position, Quaternion.identity);
         currentPreview.transform.SetParent(previewPosition, false);
     }
@@ -35,4 +57,8 @@ public class SkinController : MonoBehaviour
         Player.Instance.ApplySkin(skins[currentIndex]);
     }
 
+    private void DisplaySkinName()
+    {
+        skinName.text = skins[currentIndex].name;
+    }
 }
