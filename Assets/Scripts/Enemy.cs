d259bbf31102de5642e9c6bee5837c0486f31c46
WRKT <winness.rakotozafy@outlook.fr>
2024-05-03 10:59:00 +0200

    FIX - eagle direction animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,27 +6,43 @@ public class Enemy : MonoBehaviour
 {
     private bool isActive = false;
     [SerializeField] private float speed = 40.0f;
+    private Vector3 lastPlayerPosition;
+
     private void Start()
     {
         DeactivateEnemy();
     }
+
     private void Update()
     {
-        if (isActive && Player.Instance != null)
+        if (isActive)
         {
-            Vector3 targetPosition = Player.Instance.transform.position;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            Vector3 targetPosition;
+            if (Player.Instance != null)
+            {
+                lastPlayerPosition = Player.Instance.transform.position + new Vector3(-10, -2, 0);
+                targetPosition = lastPlayerPosition;
+            }
+            else
+            {
+                targetPosition = lastPlayerPosition;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.unscaledDeltaTime);
         }
     }
+
     public void ActivateEnemy(Vector3 playerPosition)
     {
-        if(!isActive)
+        if (!isActive)
         {
             transform.position = playerPosition + new Vector3(10, 2, 0);
             gameObject.SetActive(true);
             isActive = true;
+            lastPlayerPosition = transform.position;
         }
     }
+
     public void DeactivateEnemy()
     {
         gameObject.SetActive(false);
