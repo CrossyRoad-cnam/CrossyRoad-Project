cf8d9a089990830616003bc1bbb185c2f35ba9fc
Dinholu <alizeehett@hotmail.fr>
2024-05-09 23:35:32 +0200

    ADD - ajout de la caméra qui suit sur l'axe z seulement sur 2 et -2

--- Assets/Scripts/FollowObjectAxis.cs
+++ Assets/Scripts/FollowObjectAxis.cs
@@ -8,7 +8,10 @@ public class FollowObjectAxis : MonoBehaviour
     [SerializeField] private bool followXAxis;
     [SerializeField] private bool followYAxis;
     [SerializeField] private bool followZAxis;
-    [SerializeField] private float smoothSpeed; 
+    [SerializeField] private float smoothSpeed;
+
+    private float minZ = -2f; 
+    private float maxZ = 2f;
 
     void Update()
     {
@@ -23,11 +26,11 @@ public class FollowObjectAxis : MonoBehaviour
     /// </summary>
     private void FollowPlayerOnAxis()
     {
+
         Vector3 targetPosition = new Vector3(
             followXAxis ? player.transform.position.x : transform.position.x,
             followYAxis ? player.transform.position.y : transform.position.y,
-            followZAxis ? player.transform.position.z : transform.position.z
-        );
+            followZAxis ? Mathf.Clamp(player.transform.position.z, minZ, maxZ) : transform.position.z);
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
     }
